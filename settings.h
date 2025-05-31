0f0b9241b41daac75481f11c0fa0331bc951fd59
rondoval <rondoval@gmail.com>
2025-05-31 20:59:44 +0200

    bump version

--- settings.h
+++ settings.h
@@ -3,9 +3,9 @@
 #define _SETTINGS_H
 
 #define DEVICE_NAME          "genet.device"
-#define DEVICE_IDSTRING      "genet 1.0 (25 May 2025)"
+#define DEVICE_IDSTRING      "genet 1.1 (31 May 2025)"
 #define DEVICE_VERSION       1
-#define DEVICE_REVISION      0
+#define DEVICE_REVISION      1
 #define DEVICE_PRIORITY      -90
 
 #endif /* _SETTINGS_H */
